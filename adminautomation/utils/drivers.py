c3e9560a58cb1df4ba5ec60a3d8e0099586b5bd9
Sam Fonseca <sam@bypassmobile.com>
2015-01-23 16:51:07 -0600

    Removed comments from driver.py

--- adminautomation/utils/drivers.py
+++ adminautomation/utils/drivers.py
@@ -1,13 +1,10 @@
 from os import getenv
 from selenium import webdriver
-# from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 
 def get_chrome_driver():
     return webdriver.Remote(command_executor=getenv('SELENIUM_SERVER_URL', 'http://127.0.0.1:4444/wd/hub'),
                             desired_capabilities=webdriver.DesiredCapabilities.CHROME)
-    # return webdriver.Remote(getenv('SELENIUM_SERVER_URL', 'http://127.0.0.1:9515'),
-    #                         desired_capabilities=webdriver.DesiredCapabilities.CHROME)
 
 
 
