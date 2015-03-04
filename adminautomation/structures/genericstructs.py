c2151996d61259ecb522d3b9277bf82bb8f34ad7
Sam Fonseca <sam@bypassmobile.com>
2015-03-04 11:41:56 -0600

    Added PaginationButtons structure

--- adminautomation/structures/genericstructs.py
+++ adminautomation/structures/genericstructs.py
@@ -23,3 +23,34 @@ class DropDownSelector(object):
     def __init__(self, element):
         self.element = element
         #self.TEXT =
+
+
+class PaginationButtons(object):
+    def __init__(self, driver, button_group_locator=('css selector', 'ul.pagination'), buttons_locator=('css selector', 'li a')):
+        """A generic set of pagination buttons, usually seen at the bottom of a datatable.
+        :param ele
+        :param buttons_locator: The selector used to search for the individual buttons. Formated as a 2-item tuple
+        containing a selector type, from the By class, and match value.
+        """
+        self.driver = driver
+        self.button_group_locator = button_group_locator
+        self.buttons_locator = buttons_locator
+
+    @property
+    def BUTTON_GROUP(self):
+        return self.driver.find_element(*self.button_group_locator)
+
+    @property
+    def BUTTONS(self):
+        return self.BUTTON_GROUP.find_elements(*self.buttons_locator)
+
+    def get_last_page_button(self, last_page_button_locator=('css selector', 'a[ng-switch-when="last"]')):
+        return self.BUTTON_GROUP.find_element(*last_page_button_locator)
+
+    def get_next_page_button(self, next_page_button_locator=('css selector', 'a[ng-switch-when="next"]')):
+        return self.BUTTON_GROUP.find_element(*next_page_button_locator)
+
+    def get_previous_page_button(self, previous_page_button_locator=('css selector', 'a[ng-switch-when="prev"]')):
+        return self.BUTTON_GROUP.find_element(*previous_page_button_locator)
+
+
