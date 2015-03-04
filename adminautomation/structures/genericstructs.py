eabfc45d0cdff4afee0c4493defa3b00203964cc
Sam Fonseca <sam@bypassmobile.com>
2015-03-04 15:41:48 -0600

    Commented out unfinished classes

--- adminautomation/structures/genericstructs.py
+++ adminautomation/structures/genericstructs.py
@@ -6,23 +6,23 @@ from selenium.webdriver.support.select import Select
 from adminautomation.utils.locators import BaseLocator
 
 
-class DataTableRow(object):
-    def __init__(self, row, headers, locators):
-        self.row = row
-        self.headers = headers
-        self.locators = locators
-
-    def __setattr__(self, key, value):
-        if isinstance(value, BaseLocator):
-            object.__setattr__(self, key, object.__getattribute__('row').find_element(*value))
-        else:
-            object.__setattr__(self, key, value)
-
-class DropDownSelector(object):
-
-    def __init__(self, element):
-        self.element = element
-        #self.TEXT =
+# class DataTableRow(object):
+#     def __init__(self, row, headers, locators):
+#         self.row = row
+#         self.headers = headers
+#         self.locators = locators
+#
+#     def __setattr__(self, key, value):
+#         if isinstance(value, BaseLocator):
+#             object.__setattr__(self, key, object.__getattribute__('row').find_element(*value))
+#         else:
+#             object.__setattr__(self, key, value)
+#
+# class DropDownSelector(object):
+#
+#     def __init__(self, element):
+#         self.element = element
+#         #self.TEXT =
 
 
 class PaginationButtons(object):
