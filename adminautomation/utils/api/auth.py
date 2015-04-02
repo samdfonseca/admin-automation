c119d2f90a2cacfecd1e0e7299f16510a176713d
Sam Fonseca <sam@bypassmobile.com>
2015-04-01 19:12:14 -0500

    - Added default auth server to get_session_token

--- adminautomation/utils/api/auth.py
+++ adminautomation/utils/api/auth.py
@@ -1,7 +1,9 @@
 import requests
 import json
+from urlparse import urljoin
 
 
-def get_session_token(user, password, auth_server):
+def get_session_token(user, password, auth_server='http://auth-integration.bypasslane.com'):
+    auth_server = urljoin(auth_server, 'auth.json') if not auth_server.endswith('auth.json') else auth_server
     resp = requests.post(auth_server, auth=(user, password))
     return json.loads(resp.content)['session_token']
