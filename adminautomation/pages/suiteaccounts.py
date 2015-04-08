914fa9dbebd6852f38b66c4874b566aa9e3da9da
Sam Fonseca <sam@bypassmobile.com>
2015-04-08 12:34:08 -0500

    - Cleanup SuiteAccountsPage class

--- adminautomation/pages/suiteaccounts.py
+++ adminautomation/pages/suiteaccounts.py
@@ -1,44 +1,30 @@
 # Page object for the Suite Accounts page
 
-from adminautomation.pages import AdminPage, BasePage
-from adminautomation.utils.locators import SuiteAccountsLocators
+from adminautomation.pages import AdminPage, DataTablePage
+from adminautomation.locators import SuiteAccountsLocators
 from adminautomation.structures.suiteaccountstructs import (SuiteAccountRow,
-                                                            NewSuiteAccountForm,
-                                                            EditSuiteAccountForm,
-                                                            NewCustomerDialog,
-                                                            EditCustomerDialog)
+                                                            NewSuiteAccountForm)
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.select import Select
 from selenium.webdriver.common.alert import Alert
 
 
-class SuiteAccountsPage(AdminPage, BasePage):
+class SuiteAccountsPage(AdminPage, DataTablePage):
 
     PATH = "/suite_accounts"
+    locators = SuiteAccountsLocators
 
     @property
     def NEW_SUITE_ACCOUNT_BUTTON(self):
-        return self.get_element(SuiteAccountsLocators.NEW_SUITE_ACCOUNT_BUTTON)
-
-    @property
-    def ITEMS_PER_PAGE_SELECTOR(self):
-        return self.get_element(SuiteAccountsLocators.ITEMS_PER_PAGE_SELECTOR)
+        return self.get_element(self.locators.NEW_SUITE_ACCOUNT_BUTTON)
 
     @property
     def ITEMS_PER_PAGE_OPTIONS(self):
-        return self.get_elements(SuiteAccountsLocators.ITEMS_PER_PAGE_OPTIONS)
+        return self.get_elements(self.locators.ITEMS_PER_PAGE_OPTIONS)
 
     @property
     def SUITE_ACCOUNT_SEARCHBOX(self):
-        return self.get_element(SuiteAccountsLocators.SUITE_ACCOUNTS_SEARCHBOX)
-
-    @property
-    def DATATABLE(self):
-        return self.get_element(SuiteAccountsLocators.DATATABLE)
-
-    @property
-    def DATATABLE_HEADERS(self):
-        return self.get_elements(SuiteAccountsLocators.DATATABLE_HEADERS)
+        return self.get_element(self.locators.SUITE_ACCOUNTS_SEARCHBOX)
 
     @property
     def DATATABLE_ACCOUNT_NAME_HEADER(self):
@@ -65,17 +51,29 @@ class SuiteAccountsPage(AdminPage, BasePage):
         return filter(lambda elem: elem.text == "Edit", self.DATATABLE_HEADERS)[0]
 
     @property
-    def DATATABLE_ROWS(self):
-        trs = self.get_elements(SuiteAccountsLocators.DATATABLE_ROWS)
+    def DATATABLE_TABLE_ROWS(self):
+        trs = self.get_elements(self.locators.DATATABLE_TABLE_ROWS)
         return map(SuiteAccountRow, trs)
 
-    @property
-    def DATATABLE_FOOTER(self):
-        return self.get_element(SuiteAccountsLocators.DATATABLE_FOOTER)
+    # @property
+    # def DATATABLE(self):
+    #     return self.get_element(self.locators.DATATABLE)
 
-    @property
-    def PAGINATION_BUTTONS(self):
-        return self.get_elements(SuiteAccountsLocators.PAGINATION_BUTTONS)
+    # @property
+    # def DATATABLE_HEADERS(self):
+    #     return self.get_elements(self.locators.DATATABLE_HEADERS)
+
+    # @property
+    # def ITEMS_PER_PAGE_SELECTOR(self):
+    #     return self.get_element(self.locators.ITEMS_PER_PAGE_SELECTOR)
+
+    # @property
+    # def DATATABLE_FOOTER(self):
+    #     return self.get_element(self.locators.DATATABLE_FOOTER)
+
+    # @property
+    # def PAGINATION_BUTTONS(self):
+    #     return self.get_elements(self.locators.PAGINATION_BUTTONS)
 
     def change_items_per_page(self, target_option):
         Select(self.ITEMS_PER_PAGE_SELECTOR).select_by_visible_text(str(target_option))
@@ -136,22 +134,22 @@ class SuiteAccountsPage(AdminPage, BasePage):
         self.accept_delete_suite_account_alert()
 
     def get_accounts_by_name(self, account_name):
-        return filter(lambda row: row.ACCOUNT_NAME == account_name, self.DATATABLE_ROWS)
+        return filter(lambda row: row.ACCOUNT_NAME == account_name, self.DATATABLE_TABLE_ROWS)
 
     def get_accounts_by_email(self, email):
-        return filter(lambda row: row.EMAIL == email, self.DATATABLE_ROWS)
+        return filter(lambda row: row.EMAIL == email, self.DATATABLE_TABLE_ROWS)
 
     def get_accounts_by_suite_holder(self, suite_holder):
-        return filter(lambda row: row.SUITE_HOLDER == suite_holder, self.DATATABLE_ROWS)
+        return filter(lambda row: row.SUITE_HOLDER == suite_holder, self.DATATABLE_TABLE_ROWS)
 
     def get_accounts_by_phone(self, phone):
-        return filter(lambda row: row.PHONE == phone, self.DATATABLE_ROWS)
+        return filter(lambda row: row.PHONE == phone, self.DATATABLE_TABLE_ROWS)
 
     def get_accounts_by_suite(self, suite):
-        return filter(lambda row: row.SUITE == suite, self.DATATABLE_ROWS)
+        return filter(lambda row: row.SUITE == suite, self.DATATABLE_TABLE_ROWS)
 
     def get_account_by_id(self, id):
-        for account in self.DATATABLE_ROWS:
+        for account in self.DATATABLE_TABLE_ROWS:
             if account.ID == id:
                 return account
         else:
