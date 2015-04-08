93536e9569f0bcf9b931e588fca0993c0f8233c9
Sam Fonseca <sam@bypassmobile.com>
2015-04-08 12:30:31 -0500

    - Improvements to DataTablePage class

--- adminautomation/pages/datatablepage.py
+++ adminautomation/pages/datatablepage.py
@@ -1,4 +1,7 @@
+import re
 from selenium.webdriver.support.select import Select
+from adminautomation.locators.by import css
+from adminautomation.structures.genericstructs import PaginationButtons
 
 
 class DataTablePage(object):
@@ -12,8 +15,8 @@ class DataTablePage(object):
         return self.get_elements(self.locators.DATATABLE_HEADERS)
 
     @property
-    def DATATABLE_ROWS(self):
-        return self.get_elements(self.locators.DATATABLE_ROWS)
+    def DATATABLE_TABLE_ROWS(self):
+        return self.get_elements(self.locators.DATATABLE_TABLE_ROWS)
 
     @property
     def DATATABLE_FOOTER(self):
@@ -21,8 +24,37 @@ class DataTablePage(object):
 
     @property
     def PAGINATION_BUTTONS(self):
-        return self.get_elements(self.locators.PAGINATION_BUTTONS)
+        return PaginationButtons(self.locators.PAGINATION_BUTTONS)
+
+    @property
+    def TOTAL_ITEMS_STRONG(self):
+        try:
+            return self.get_element(self.locators.TOTAL_ITEMS_STRONG)
+        except AttributeError:
+            return None
 
     @property
     def ITEMS_PER_PAGE_SELECTOR(self):
-        return Select(self.get_element(self.locators.ITEMS_PER_PAGE_SELECTOR))
+        try:
+            return Select(self.get_element(self.locators.ITEMS_PER_PAGE_SELECTOR))
+        except AttributeError:
+            return None
+
+    @property
+    def total_items_count(self):
+        total_items_full_text = self.TOTAL_ITEMS_STRONG.text
+        if total_items_full_text is None:
+            return None
+        regex = re.compile('[0-9]+')
+        try:
+            return regex.search(total_items_full_text).group(0)
+        except AttributeError:
+            return None
+
+    def get_column_by_header_text(self, header_text):
+        return self.get_elements(self.locators.DATATABLE + css('td[data-title-text="{0}"]'.format(header_text)))
+
+    def filter_rows_by_value(self, header_text, filter_value):
+        locator = css('td[data-title-text"{0}"]'.format(header_text))
+        return filter(lambda row: row.find_element(*locator).text == filter_value, self.DATATABLE_TABLE_ROWS)
+
