0df8b7d47eeaebbfb6b1a8e17d9a3d3ac5e2b863
Sam Fonseca <sam@bypassmobile.com>
2015-04-08 08:37:32 -0500

    - Updated auth api util to raise HTTPError on unsuccessful request

--- adminautomation/utils/api/auth.py
+++ adminautomation/utils/api/auth.py
@@ -6,4 +6,6 @@ from urlparse import urljoin
 def get_session_token(user, password, auth_server='http://auth-integration.bypasslane.com'):
     auth_server = urljoin(auth_server, 'auth.json') if not auth_server.endswith('auth.json') else auth_server
     resp = requests.post(auth_server, auth=(user, password))
-    return json.loads(resp.content)['session_token']
+    resp.raise_for_status()
+    token = json.loads(resp.content)['session_token']
+    return token
