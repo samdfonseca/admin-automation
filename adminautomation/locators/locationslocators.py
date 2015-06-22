e843da59b31de441179dd62784d12b3bf39cf48d
Sam Fonseca <sam@bypassmobile.com>
2015-06-22 12:31:08 -0500

    First shot at new location form

--- adminautomation/locators/locationslocators.py
+++ adminautomation/locators/locationslocators.py
@@ -1,7 +1,34 @@
+from adminautomation.locators import BaseLocatorGroup, BaseLocator
 from adminautomation.locators.datatablelocators import DataTableLocators
 from adminautomation.locators.by import css
+from adminautomation.structures import Select2
 
 
 class LocationsLocators(DataTableLocators):
     EXPORT_BUTTON = css('a.btn.blue')
     NEW_LOCATION_BUTTON = css('a.btn.green')
+
+class NewLocationFormLocators(BaseLocatorGroup):
+    FORM = css('form[action="/locations"]')
+    NAME_INPUT = css('#location_name')
+    DESCRIPTION_INPUT = css('#location_description')
+    TYPE_SELECT = css('#location_type')
+    LOCATION_GROUP_SELECT = css('#location_location_group_id')
+    DEFAULT_TRANSFER_SOURCE_SELECT = css('#location_default_transfer_source_id')
+    LOCATION_TAGS_INPUT = css('#location_tag_list')
+    CLOSED_RADIO_BUTTON = css('#concessionClosed')
+    OPEN_RADIO_BUTTON = css('#concessionOpen')
+    INSEAT_DELIVERY_CHECKBOX = css('#location_has_inseat')
+    HAS_PICKUP_CHECKBOX = css('#location_has_pickup')
+    MERCHANDISE_CHECKBOX = css('#location_is_merchandise')
+    PAYMENT_TYPE_CHECKBOXES = css('input#location_tender_accepted_')
+    PRINT_RECEIPT_TWICE_CHECKBOX = css('#location_print_receipt_twice')
+    AUTO_PRINT_CASH_ORDERS_CHECKBOX = css('#location_auto_print_cash')
+    ACCEPT_DIGITAL_SIGNATURES_CHECKBOX = css('#location_signature_on_tablet')
+    AUTO_PRINT_CREDIT_ORDERS_CHECKBOX = css('#location_auto_print_credit')
+    AUTO_PRINT_REMOTE_ORDERS_CHECKBOX = css('#location_auto_print_remote_orders')
+    AUTO_PRINT_OTHER_ORDERS_CHECKBOX = css('#location_auto_print_other')
+    ALLOW_TIPS_CHECKBOX = css('#location_allow_tips')
+    ENABLE_EMAIL_RECEIPTS_CHECKBOX = css('#location_email_receipts_enabled')
+    CANCEL_BUTTON = css('a.btn.grey[href="/locations"]')
+    SAVE_LOCATION_BUTTON = css('input.btn.light-blue[value="Save Location"]')
--- adminautomation/pages/locations.py
+++ adminautomation/pages/locations.py
@@ -1,7 +1,10 @@
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.select import Select
+from selenium.webdriver.common.alert import Alert
 from adminautomation.pages import AdminPage, DataTablePage
-from adminautomation.locators.locationslocators import LocationsLocators
+from adminautomation.locators.locationslocators import LocationsLocators, NewLocationFormLocators
 from adminautomation.locators.by import link_text
+from adminautomation.structures import Select2
 
 
 class LocationsPage(AdminPage, DataTablePage):
@@ -21,12 +24,17 @@ class LocationsPage(AdminPage, DataTablePage):
     def export_button(self):
         return self.get_element(self.locators.EXPORT_BUTTON)
 
+    @property
+    def alert(self):
+        return Alert(self.driver)
+
     def open_new_location_form(self):
         self.new_location_button.click()
         self.wait_for_page_title('Locations - New', timeout=10)
 
     def search_for_location(self, query):
         self.filter_table('Name', query)
+        self.wait_for_elements(self.locators.DATATABLE_TABLE_ROWS)
 
     def filter_table_by_type(self, location_type):
         self.show_filters()
@@ -52,15 +60,228 @@ class LocationsPage(AdminPage, DataTablePage):
         timeout = kwargs.get('timeout', 30)
         self.wait_for_elements(ref_element_locator, timeout=timeout)
 
-    def get_row_by_name(self, item_name):
-        self.wait_for_element(link_text(item_name))
-        return self.filter_rows_by_value('Name', item_name)[0]
+    def get_row_by_name(self, location_name):
+        elem = filter(lambda i: ' '.join(i.text.split()[:-4]) == location_name, self.DATATABLE_TABLE_ROWS)[0]
+        return elem
 
     def click_edit_location_link_by_name(self, location_name):
-        elem = filter(lambda i: ' '.join(i.text.split()[:-4]) == location_name, self.DATATABLE_TABLE_ROWS)[0]
+        elem = self.get_row_by_name(location_name)
         elem.find_element_by_link_text('Edit').click()
 
     def click_delete_location_link_by_name(self, location_name):
-        elem = filter(lambda i: ' '.join(i.text.split()[:-4]) == location_name, self.DATATABLE_TABLE_ROWS)[0]
+        elem = self.get_row_by_name(location_name)
         elem.find_element_by_link_text('Delete').click()
+
+    def accept_alert(self):
+        self.alert.accept()
+
+    def dismiss_alert(self):
+        self.alert.dismiss()
+
+
+class NewLocationPage(AdminPage):
+    PATH = 'locations/new'
+    locators = NewLocationFormLocators
+
+    @property
+    def name_input(self):
+        return self.get_element(self.locators.NAME_INPUT)
+
+    @property
+    def description_input(self):
+        return self.get_element(self.locators.DESCRIPTION_INPUT)
+
+    @property
+    def type_select(self):
+        return Select2(self.get_element(self.locators.TYPE_SELECT))
+
+    @property
+    def location_group_select(self):
+        return Select2(self.get_element(self.locators.LOCATION_GROUP_SELECT))
+
+    @property
+    def default_transfer_source_select(self):
+        return Select2(self.get_element(self.locators.DEFAULT_TRANSFER_SOURCE_SELECT))
+
+    @property
+    def location_tags_input(self):
+        return self.get_element(self.locators.LOCATION_TAGS_INPUT)
+
+    @property
+    def closed_radio_button(self):
+        return self.get_element(self.locators.CLOSED_RADIO_BUTTON)
+
+    @property
+    def open_radio_button(self):
+        return self.get_element(self.locators.OPEN_RADIO_BUTTON)
+
+    @property
+    def inseat_delivery_checkbox(self):
+        return self.get_element(self.locators.INSEAT_DELIVERY_CHECKBOX)
+
+    @property
+    def has_pickup_checkbox(self):
+        return self.get_element(self.locators.HAS_PICKUP_CHECKBOX)
+
+    @property
+    def merchandise_checkbox(self):
+        return self.get_element(self.locators.MERCHANDISE_CHECKBOX)
+
+    @property
+    def payment_type_checkboxes(self):
+        return self.get_elements(self.locators.PAYMENT_TYPE_CHECKBOXES)
+
+    @property
+    def print_receipts_twice_checkbox(self):
+        return self.get_element(self.locators.PRINT_RECEIPT_TWICE_CHECKBOX)
+
+    @property
+    def auto_print_cash_orders_checkbox(self):
+        return self.get_element(self.locators.AUTO_PRINT_CASH_ORDERS_CHECKBOX)
+
+    @property
+    def accept_digital_signatures_checkbox(self):
+        return self.get_element(self.locators.ACCEPT_DIGITAL_SIGNATURES_CHECKBOX)
+
+    @property
+    def auto_print_credit_orders_checkbox(self):
+        return self.get_element(self.locators.AUTO_PRINT_CREDIT_ORDERS_CHECKBOX)
+
+    @property
+    def auto_print_remote_orders_checkbox(self):
+        return self.get_element(self.locators.AUTO_PRINT_REMOTE_ORDERS_CHECKBOX)
+
+    @property
+    def auto_print_other_orders_checkbox(self):
+        return self.get_element(self.locators.AUTO_PRINT_OTHER_ORDERS_CHECKBOX)
+
+    @property
+    def allow_tips_checkbox(self):
+        return self.get_element(self.locators.ALLOW_TIPS_CHECKBOX)
+
+    @property
+    def enable_email_receipts_checkbox(self):
+        return self.get_element(self.locators.ENABLE_EMAIL_RECEIPTS_CHECKBOX)
+
+    @property
+    def cancel_button(self):
+        return self.get_element(self.locators.CANCEL_BUTTON)
+
+    @property
+    def save_location_button(self):
+        return self.get_element(self.locators.SAVE_LOCATION_BUTTON)
+
+    def enter_name(self, name):
+        self.name_input.clear()
+        self.name_input.send_keys(name)
+
+    def enter_description(self, description):
+        self.description_input.clear()
+        self.description_input.send_keys(description)
+
+    def select_location_type(self, type):
+        self.type_select.select_by_visible_text(type)
+
+    def select_location_group(self, group):
+        self.location_group_select.select_by_visible_text(group)
+
+    def clear_location_group(self):
+        self.location_group_select.clear()
+
+    def select_default_transfer_source(self, source):
+        self.default_transfer_source_select.select_by_visible_text(source)
+
+    def clear_default_transfer_source(self):
+        self.default_transfer_source_select.clear()
+
+    def add_location_tag(self, tag):
+        self.location_tags_input.send_keys(tag)
+
+    def clear_location_tags(self):
+        for elem in self.location_tags_input.get_elements('a.select2-search-choice-close'):
+            elem.click()
+        i = 0
+        while self.location_tags_input.text != '':
+            self.location_tags_input.send_keys(Keys.BACK_SPACE)
+            i += 1
+            if i > 100:
+                break
+
+    def remove_location_tag(self, tag):
+        elems = self.location_tags_input.get_elements('li.select2-search-choice')
+        try:
+            elem = filter(lambda e: e.text == tag, elems)[0]
+        except IndexError:
+            return
+        elem.find_element_by_tag_name('a').click()
+
+    def set_status_closed(self):
+        if self.is_open:
+            self.closed_radio_button.click()
+
+    def set_status_open(self):
+        if self.is_closed:
+            self.open_radio_button.click()
+
+    @property
+    def is_closed(self):
+        return self.closed_radio_button.is_selected()
+
+    @property
+    def is_open(self):
+        return self.open_radio_button.is_selected()
+
+    @property
+    def has_inseat_delivery(self):
+        return self.inseat_delivery_checkbox.is_selected()
+
+    def check_inseat_delivery(self):
+        if not self.has_inseat_delivery:
+            self.inseat_delivery_checkbox.click()
+
+    def uncheck_inseat_delivery(self):
+        if self.has_inseat_delivery:
+            self.inseat_delivery_checkbox.click()
+
+    @property
+    def has_pickup(self):
+        return self.has_pickup_checkbox.is_selected()
+
+    def check_has_pickup(self):
+        if not self.has_pickup:
+            self.has_pickup_checkbox.click()
+
+    def uncheck_has_pickup(self):
+        if self.has_pickup:
+            self.has_pickup_checkbox.click()
+
+    @property
+    def is_merchandise(self):
+        return self.merchandise_checkbox.is_selected()
+
+    def check_merchandise(self):
+        if not self.is_merchandise:
+            self.merchandise_checkbox.click()
+
+    def uncheck_merchandise(self):
+        if self.is_merchandise:
+            self.merchandise_checkbox.click()
+
+    def _get_payment_checkbox_by_name(self, payment_type):
+        try:
+            return filter(lambda e: e.find_element_by_xpath('../../..').text == payment_type, self.payment_type_checkboxes)[0]
+        except IndexError:
+            return None
+
+    def payment_type_is_enabled(self, payment_type):
+        elem = self._get_payment_checkbox_by_name(payment_type)
+        return elem.is_selected()
+
+    def check_payment_type(self, payment_type):
+        if not self.payment_type_is_enabled(payment_type):
+            self._get_payment_checkbox_by_name(payment_type).click()
+
+    def uncheck_payment_type(self, payment_type):
+        if self.payment_type_is_enabled(payment_type):
+            self._get_payment_checkbox_by_name(payment_type).click()
 
